1bfe3c80ab5c6cbfb093db6794b1c034a4571fbe
影浅 <soyz@qq.com>
2018-03-05 13:49:50 +0800

    weapp-extra v2.0.5

--- template/utils/extra.js
+++ template/utils/extra.js
@@ -1,210 +1,275 @@
 /*!
- * weapp-extra v2.0.2 (https://www.npmjs.com/package/weapp-extra)
+ * weapp-extra v2.0.5 (https://www.npmjs.com/package/weapp-extra)
  * Copyright 2018 seekwe@gmail.com, Inc.
  * Licensed under the Apache-2.0 license
  */
 "use strict"
 var _extends =
     Object.assign ||
-    function(a) {
-      for (var c, b = 1; b < arguments.length; b++)
-        for (var d in ((c = arguments[b]), c))
-          Object.prototype.hasOwnProperty.call(c, d) && (a[d] = c[d])
-      return a
+    function(n) {
+      for (var t = 1; t < arguments.length; t++) {
+        var i = arguments[t]
+        for (var e in i)
+          Object.prototype.hasOwnProperty.call(i, e) && (n[e] = i[e])
+      }
+      return n
     },
-  api = function(a, b) {
+  requestLimit = void 0,
+  config = { processes: 10 }
+Promise.prototype.finally = function(n) {
+  function t() {
+    setTimeout(n)
+  }
+  return this.then(t, t), this
+}
+var api = function(n, t) {
     for (
-      var c = arguments.length, d = Array(2 < c ? c - 2 : 0), f = 2;
-      f < c;
-      f++
+      var i = arguments.length, e = Array(i > 2 ? i - 2 : 0), o = 2;
+      o < i;
+      o++
     )
-      d[f - 2] = arguments[f]
-    var g = "function" == typeof wx[a]
-    return new Promise(function(h, j) {
-      if (g) {
-        var m
-        ;(m = wx)[a].apply(
-          m,
-          [_extends({}, b, { success: h, fail: j })].concat(d)
-        )
-      } else j({ errMsg: "API:" + a + "\u4E0D\u5B58\u5728\uFF0C\u8BF7\u5C1D\u8BD5\u5347\u7EA7\u5FAE\u4FE1", type: "version" })
+      e[o - 2] = arguments[o]
+    var c = "function" == typeof wx[n]
+    return new Promise(function(i, o) {
+      var r = { success: i, fail: o }
+      if (c)
+        if (customize[n])
+          customize[n].apply(customize, [_extends({}, t, r)].concat(e))
+        else {
+          var u
+          ;(u = wx)[n].apply(u, [_extends({}, t, r)].concat(e))
+        }
+      else o({ errMsg: "API:" + n + "不存在，请尝试升级微信", type: "version" })
     })
   },
-  openSetting = function(a) {
-    return new Promise(function(b, c) {
+  openSetting = function(n) {
+    return new Promise(function(t, i) {
       api("openSetting")
-        .then(function(d) {
-          getSetting(a, d)
-            .then(function(f) {
-              f ? b(f) : c(f)
+        .then(function(e) {
+          getSetting(n, e)
+            .then(function(n) {
+              n ? t(n) : i(n)
             })
-            .catch(function(f) {
-              c(f)
+            .catch(function(n) {
+              i(n)
             })
         })
-        .catch(function(d) {
-          c(d)
+        .catch(function(n) {
+          i(n)
         })
     })
   },
-  getSetting = function(a, b) {
-    var c = function isTrue(f) {
-        return f && (!(!0 !== a) || f.authSetting[a])
-      },
-      d = new Promise(function(f) {
-        b
-          ? f(c(b))
-          : api("getSetting")
-              .then(function(h) {
-                f(c(h))
-              })
-              .catch(function(h) {
-                f(h)
-              })
-      })
-    return d
+  getSetting = function(n, t) {
+    var i = function(t) {
+      return t && (!0 === n || t.authSetting[n])
+    }
+    return new Promise(function(n, e) {
+      t
+        ? n(i(t))
+        : api("getSetting")
+            .then(function(t) {
+              n(i(t))
+            })
+            .catch(function(t) {
+              n(t)
+            })
+    })
   },
   login = function() {
-    var a = 0 < arguments.length && arguments[0] !== void 0 ? arguments[0] : !0,
-      b = 1 < arguments.length && arguments[1] !== void 0 ? arguments[1] : 1,
-      c = 2 < arguments.length && arguments[2] !== void 0 ? arguments[2] : {},
-      d = 3 < arguments.length && arguments[3] !== void 0 ? arguments[3] : null
-    return new Promise(function(f, g) {
-      var h = function() {
+    var n = !(arguments.length > 0 && void 0 !== arguments[0]) || arguments[0],
+      t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : 1,
+      i = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : {},
+      e = arguments.length > 3 && void 0 !== arguments[3] ? arguments[3] : null
+    return new Promise(function(o, c) {
+      var r = function() {
         wx.login({
-          success: function success(j) {
-            var k = j.code
-            0 === b,
-              authApi("getUserInfo", "scope.userInfo", b, c, d)
-                .then(function(m) {
-                  ;(m.code = k), f(m)
-                })
-                .catch(function(m) {
-                  ;(m.code = k), g(m)
-                })
+          success: function(n) {
+            var r = n.code
+            authApi("getUserInfo", "scope.userInfo", t, i, e)
+              .then(function(n) {
+                ;(n.code = r), o(n)
+              })
+              .catch(function(n) {
+                ;(n.code = r), c(n)
+              })
           },
-          fail: function fail(j) {
-            g(j)
+          fail: function(n) {
+            c(n)
           }
         })
       }
-      a
+      n
         ? wx.checkSession({
-            success: function success() {
-              f(!0)
+            success: function(n) {
+              o(!0)
             },
-            fail: function fail() {
-              h()
+            fail: function() {
+              r()
             }
           })
-        : h()
+        : r()
     })
   },
-  authApi = function(a) {
+  authApi = function(n) {
     for (
-      var b = arguments.length, c = Array(6 < b ? b - 6 : 0), d = 6;
-      d < b;
-      d++
+      var t = arguments.length, i = Array(t > 6 ? t - 6 : 0), e = 6;
+      e < t;
+      e++
     )
-      c[d - 6] = arguments[d]
-    var f =
-        1 < arguments.length && arguments[1] !== void 0 ? arguments[1] : null,
-      g = 2 < arguments.length && arguments[2] !== void 0 ? arguments[2] : 1,
-      h = 3 < arguments.length && arguments[3] !== void 0 ? arguments[3] : {},
-      j = 4 < arguments.length && arguments[4] !== void 0 ? arguments[4] : null,
-      k = arguments[5]
-    f || (f = !0)
-    var m,
-      n,
-      o = new Promise(function(p, q) {
-        ;(n = function() {
-          openSetting(f)
-            .then(function() {
-              r()
-            })
-            .catch(function(t) {
-              "version" === t.type
-                ? q(t)
-                : -1 === g
-                  ? s(m)
-                  : j
-                    ? wx.showModal(
-                        Object.assign(
-                          {
-                            title: "\u63D0\u793A",
-                            confirmText: "\u786E\u5B9A",
-                            content:
-                              "\u5BF9\u5E94\u6743\u9650\u6CA1\u6709\u5F00\u542F,\u90E8\u5206\u529F\u80FD\u53D7\u9650",
-                            showCancel: !1,
-                            complete: function complete() {
-                              q(m)
-                            }
-                          },
-                          "string" == typeof j
-                            ? { content: j }
-                            : !0 === j ? {} : j
-                        )
+      i[e - 6] = arguments[e]
+    var o =
+        arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : null,
+      c = arguments.length > 2 && void 0 !== arguments[2] ? arguments[2] : 1,
+      r = arguments.length > 3 && void 0 !== arguments[3] ? arguments[3] : {},
+      u = arguments.length > 4 && void 0 !== arguments[4] ? arguments[4] : null,
+      f = arguments[5]
+    o || (o = !0)
+    var s = void 0,
+      a = void 0
+    return new Promise(function(t, e) {
+      ;(a = function() {
+        openSetting(o)
+          .then(function(n) {
+            l()
+          })
+          .catch(function(n) {
+            "version" === n.type
+              ? e(n)
+              : -1 === c
+                ? g(s)
+                : u
+                  ? wx.showModal(
+                      Object.assign(
+                        {
+                          title: "提示",
+                          confirmText: "确定",
+                          content: "对应权限没有开启,部分功能受限",
+                          showCancel: !1,
+                          complete: function() {
+                            e(s)
+                          }
+                        },
+                        "string" == typeof u
+                          ? { content: u }
+                          : !0 === u ? {} : u
                       )
-                    : q(m)
-            })
-        }),
-          (h =
-            !!h &&
-            Object.assign(
-              {
-                title: "\u63D0\u793A",
-                confirmText: "\u786E\u5B9A",
-                content: "\u8BF7\u5148\u5F00\u542F\u5BF9\u5E94\u6743\u9650",
-                showCancel: !1
+                    )
+                  : e(s)
+          })
+      }),
+        (r =
+          !!r &&
+          Object.assign(
+            {
+              title: "提示",
+              confirmText: "确定",
+              content: "请先开启对应权限",
+              showCancel: !1
+            },
+            "string" == typeof r ? { content: r } : !0 === r ? {} : r,
+            {
+              success: function(n) {
+                !0 !== n.cancel && a()
               },
-              "string" == typeof h ? { content: h } : !0 === h ? {} : h,
-              {
-                success: function success(t) {
-                  !0 !== t.cancel && n()
-                },
-                fail: function fail() {}
-              }
-            ))
-        var r = function success() {
-            api
-              .apply(void 0, [a, k].concat(c))
-              .then(function(t) {
-                p(t)
-              })
-              .catch(function(t) {
-                q(t)
-              })
-          },
-          s = function fail(t) {
-            m || (m = t)
-            t.type
-            h ? wx.showModal(h) : n()
-          }
-        api
-          .apply(void 0, [a, k].concat(c))
-          .then(function(t) {
-            p(t)
-          })
-          .catch(function(t) {
-            var u = t.type || "warn"
-            ;(t = _extends({ type: u }, t)),
-              "version" !== u && 0 !== g ? s(t) : q(t)
-          })
-      })
-    return o
+              fail: function(n) {}
+            }
+          ))
+      var l = function() {
+          api
+            .apply(void 0, [n, f].concat(i))
+            .then(function(n) {
+              t(n)
+            })
+            .catch(function(n) {
+              e(n)
+            })
+        },
+        g = function(n) {
+          s || (s = n)
+          n.type
+          r ? wx.showModal(r) : a()
+        }
+      api
+        .apply(void 0, [n, f].concat(i))
+        .then(function(n) {
+          t(n)
+        })
+        .catch(function(n) {
+          var t = n.type || "warn"
+          ;(n = _extends({ type: t }, n)),
+            "version" !== t && 0 !== c ? g(n) : e(n)
+        })
+    })
   },
-  runCount = function(a, b) {
-    var c = 2 < arguments.length && arguments[2] !== void 0 && arguments[2]
+  runCount = function(n, t) {
+    var i = arguments.length > 2 && void 0 !== arguments[2] && arguments[2]
     return function() {
-      a--, (c ? 0 === a : 0 >= a) && b()
+      n--, (i ? 0 === n : n <= 0) && t()
     }
   },
   runInit = function() {
-    var a = []
-    return function(b, c) {
-      if ((a.push(b), c))
-        for (var g, d = 0, f = a.length; d < f; d++) (g = a.shift()), g && g(c)
+    var n = []
+    return function(t, i) {
+      if ((n.push(t), i))
+        for (var e = 0, o = n.length; e < o; e++) {
+          var c = n.shift()
+          c && c(i)
+        }
     }
+  },
+  runLimit = function(n) {
+    var t = [],
+      i = 0,
+      e = function() {
+        i--, o()
+      },
+      o = function() {
+        if (t.length > 0) {
+          t.shift()(e)
+        }
+      }
+    return function(e) {
+      i++, t.push(e), i <= n && o()
+    }
+  },
+  customize = {
+    downloadFile: function(n) {
+      for (
+        var t = arguments.length, i = Array(t > 1 ? t - 1 : 0), e = 1;
+        e < t;
+        e++
+      )
+        i[e - 1] = arguments[e]
+      this.processes.apply(this, ["downloadFile", n].concat(i))
+    },
+    request: function(n) {
+      for (
+        var t = arguments.length, i = Array(t > 1 ? t - 1 : 0), e = 1;
+        e < t;
+        e++
+      )
+        i[e - 1] = arguments[e]
+      this.processes.apply(this, ["request", n].concat(i))
+    },
+    processes: function(n, t) {
+      for (
+        var i = arguments.length, e = Array(i > 2 ? i - 2 : 0), o = 2;
+        o < i;
+        o++
+      )
+        e[o - 2] = arguments[o]
+      requestLimit || (requestLimit = runLimit(config.processes)),
+        requestLimit(function(i) {
+          var o
+          ;(t.complete = i), (o = wx)[n].apply(o, [t].concat(e))
+        })
+    }
+  },
+  setConfig = function(n) {
+    config = Object.assign(config, n)
+  },
+  getConfig = function(n) {
+    return config
   }
 module.exports = {
   api: api,
@@ -212,5 +277,8 @@ module.exports = {
   login: login,
   getSetting: getSetting,
   runCount: runCount,
-  runInit: runInit
+  runInit: runInit,
+  runLimit: runLimit,
+  setConfig: setConfig,
+  getConfig: getConfig
 }
